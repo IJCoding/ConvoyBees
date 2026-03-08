c4f317e031caed89b6034e0cd27c5c1c661817c0
IJCoding <jonesyirg@gmail.com>
2026-03-08 23:17:01 +0000

    added operator overloads for utils

--- Quest.h
+++ Quest.h
@@ -9,8 +9,31 @@ public:
 	Quest() {};
 	~Quest() {};
 
+	bool tryGiveRequirement(string _type, USI _amount)
+	{
+		if (requirements.contains(_type))
+			if (!requirements.at(_type).isAtMax())
+				updateRequirement(_type, _amount);
+	}
+
+	USI updateRequirement(string _type, USI _amount)
+	{
+		USI _localAmount = _amount;
+		while (!requirements.at(_type).isAtMax() && (_localAmount != 0))
+		{
+			requirements.at(_type)++;
+			_localAmount--;
+		}
+		requirements.at(_type).remainingToMax();
+		_amount;
+
+
+	};
+
+
 private:
 
+	map<string, BoundValue> requirements;
 };
 
 

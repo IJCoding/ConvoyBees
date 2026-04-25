440ffa988d25895837b1dcfc9ff9f40ebca753ac
IJCoding <jonesyirg@gmail.com>
2026-04-26 00:43:28 +0100

    added comments to make the source more legible when e not having read it in some time

--- Character.h
+++ Character.h
@@ -7,73 +7,137 @@ class Character
 {
 public:
 
+	//Checks if a character has a quest (searched for by name)
 	bool hasQuest(string _questName) const { return allQuests.contains(_questName); }
 
+	//Retun a non const reference to a given quest that the character has
 	Quest& getQuest(const string& questName) { return allQuests.at(questName); }
 
+	//Return a const reference to a given quest that the character has
 	const Quest& getQuest(const string& questName) const {return allQuests.at(questName); }
 
+	//Checks if the character has an active quest given by name
 	bool hasActiveQuest(string _questName, bool _isActive) 
 	{
-		bool result = false;
-		if (hasQuest(_questName)) result = getQuest(_questName).getIsActive() ; 
-		return result;
+		compareActiveStateOfQuest(_questName, true);
 	}
 
-	bool hasCompletedQuest(string _questName, bool _hasCompleted)
+	//Checks if the character has a quest given by name but is inactive
+	bool hasInactiveQuest(string _questName, bool _isActive)
 	{
-		bool result = false;
-		if (hasQuest(_questName)) result = getQuest(_questName).getIsCompleted();
-		return result;
+		compareActiveStateOfQuest(_questName, false);
 	}
 
+	//Checks that a quest has been completed
+	bool hasCompletedQuest(string _questName)
+	{
+		compareQuestCompletion(_questName, true);
+	}
+
+	//Checks that a quest has not been completed
+	bool hasNotCompletedQuest(string _questName)
+	{
+		compareQuestCompletion(_questName, false);
+	}
+
+	//If we do not currently have the given quest, add the quest
 	void makeQuest(Quest _quest)
 	{
-		if (hasQuest(_quest.getName()))
+		if (!hasQuest(_quest.getName()))
 		{
 			allQuests.emplace(_quest.getName(), _quest);
 		}
 	}
 
+	//Marks a given quest as complete
 	void markQuestComplete(const string& _questName)
 	{
-		if (!hasQuest(_questName))
-			return;
-
-		auto& quest = getQuest(_questName);
-
-		if (!quest.getIsCompleted())
-			quest.toggleQuestCompleted();
+		markActiveStatusOfQuest(_questName, true);
 	}
 
+	//Marks a given quest as incomplete
+	void markQuestInComplete(const string& _questName)
+	{
+		markActiveStatusOfQuest(_questName, false);
+	}
+
+	//Marks a given quest as active
 	void markQuestActive(const string& _questName)
 	{
-		if (!hasQuest(_questName))
-			return;
-
-		auto& quest = getQuest(_questName);
-
-		if (!quest.getIsActive())
-			quest.toggleQuestActive();
+		markActiveStatusOfQuest(_questName, true);
 	}
 	
+	//Marks a given quest as inactive
 	void markQuestInactive(const string& _questName)
 	{
-		if (!hasQuest(_questName))
-			return;
-
-		auto& quest = getQuest(_questName);
-
-		if (quest.getIsActive())
-			quest.toggleQuestActive();
+		markActiveStatusOfQuest(_questName, false);
 	}
+
+	//Checks if the character is active
 	bool getIsActive() const { return isActive; }
+
+	//Returns the name of the character
 	string getName() const { return name; }
+
+	//Toggles if this character is active
 	void toggleCharacterActive() { isActive = !getIsActive(); }
 
 private:
+
+	//Sets the active-state of a given quest(found by name) to the given active-state
+	void markActiveStatusOfQuest(const string& _questName, bool shouldBeActive)
+	{
+		if (!hasQuest(_questName))
+			return;
+
+		Quest& quest = getQuest(_questName);
+
+		if (!shouldBeActive)
+			quest.toggleQuestActive();
+
+	}
+
+	//Sets the completed-state of a given quest(found by name) to the given active-state
+	void markActiveStatusOfQuest(const string& _questName, bool shouldBeCompleted)
+	{
+		if (!hasQuest(_questName))
+			return;
+
+		auto& quest = getQuest(_questName);
+
+		if (!shouldBeCompleted)
+			quest.toggleQuestCompleted();
+	}
+
+	//Checks the completed-state of a given quest(found by name) to a given bool
+	bool compareQuestCompletion(string _questName, bool _isCompleted)
+	{
+		bool result = false;
+		if (_isCompleted)
+			if (hasQuest(_questName)) result = getQuest(_questName).getIsCompleted();
+		else 
+			if (hasQuest(_questName)) result = !getQuest(_questName).getIsCompleted();
+		return result;
+	}
+
+	//Checks the act of a given quest(found by name) to a given bool
+	bool compareActiveStateOfQuest(string _questName, bool _isActive)
+	{
+		bool result = false;
+		if (_isActive)
+			if (hasQuest(_questName)) result = getQuest(_questName).getIsActive();
+		else
+			if (hasQuest(_questName)) result = !getQuest(_questName).getIsActive();
+		return result;
+	}
+
+	//Name of the character
 	string name;
+
+	//Whether the character is active
 	bool isActive = false;
+
+	//A map of all quests the character is related to
 	map<string, Quest> allQuests;
 
 };
--- Settlement.h
+++ Settlement.h
@@ -7,13 +7,17 @@ class Settlement
 public:
 	Settlement() {};
 	~Settlement() {};
-
+	
+	//Checks if a given character exists (by giving the name of the character)
 	bool hasCharacter(string _characterName) const { return allCharacters.contains(_characterName); }
 
+	//Returns a given character (by searching for it's name)
 	Character& getCharacter(const string& _characterName) { return allCharacters.at(_characterName); }
 
+	//Get a const version of a given character (by searching for it's name)
 	const Character& getCharacter(const string& _characterName) const { return allCharacters.at(_characterName); }
 
+	//Checks if a given character exists and is active (by searching for it's name)
 	bool hasActiveCharacter(string _characterName, bool _isActive)
 	{
 		bool result = false;
@@ -21,38 +25,44 @@ public:
 		return result;
 	}
 
+	//Creates a character if it does not currently exist within all characters in a given settlement
 	void makeCharacter(Character _character)
 	{
-		if (hasCharacter(_character.getName()))
+		if (!hasCharacter(_character.getName()))
 		{
 			allCharacters.emplace(_character.getName(), _character);
 		}
 	}
 
+	//When given a characters' name, mark it as active (if it is currently inactive)
 	void markCharacterActive(const string& _characterName)
 	{
-		if (!hasCharacter(_characterName))
-			return;
-
-		auto& character = getCharacter(_characterName);
-
-		if (!character.getIsActive())
-			character.toggleCharacterActive();
+		markCharacterStatus(_characterName, true);
 	}	
+
+	//When given a characters' name, mark it as inactive (if it is currently inactive)
 	void markCharacterInactive(const string& _characterName)
 	{
-		if (!hasCharacter(_characterName))
-			return;
-
-		auto& character = getCharacter(_characterName);
-
-		if (character.getIsActive())
-			character.toggleCharacterActive();
+		markCharacterStatus(_characterName, false);
 	}
 
+	//Returns all attributes for the settlement
 	const map<string, BoundValue>& getAttributes() const { return attributes; }
 
 private:
+
+	//Sets the active-state of a given character(found by name) to the given active-state
+	void markCharacterStatus(const string& _characterName, bool shouldBeActive)
+	{
+		if (!hasCharacter(_characterName))
+			return;
+
+		Character& character = getCharacter(_characterName);
+
+		if (!shouldBeActive)
+			character.toggleCharacterActive();
+	}
+
 	map<string, BoundValue> attributes;
 	map<string, Character> allCharacters;
 
